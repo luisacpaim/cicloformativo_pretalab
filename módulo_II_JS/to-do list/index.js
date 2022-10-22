f149c690ee99ea04b00d398c3da9ee125f04ffcc
Luísa Cristina Ferreira <luisac.paim@gmail.com>
2022-10-22 11:27:50 -0300

    aula 22/out - localStorage todo-list

--- módulo_II_JS/to-do list/index.js
+++ módulo_II_JS/to-do list/index.js
@@ -25,17 +25,30 @@ const tarefa = (id, novaTarefa) => ` <div id="${id}">
                             <input type="checkbox" />
                         </div>` 
 
+function exibirLista() {
+    id++;
+    const tarefas = JSON.parse(localStorage.getItem('lista-tarefas'));
+    if (tarefas) {
+        tarefas.forEach(tarefaListada => {
+            document.querySelector('#tasksList').innerHTML += tarefa(id, tarefaListada)
+        });
+    }
+}
+
 function adicionarTarefa() {
     id++;
     const novaTarefa = document.getElementById('nome-tarefa').value;
-    document.querySelector('#lista-tarefas').innerHTML += tarefa(id, novaTarefa);
+    document.querySelector('#tasksList').innerHTML += tarefa(id, novaTarefa);
+    //'persiste as tarefas': add em localStorage
+    const listaTarefas = localStorage.getItem('lista-tarefas'); //cria listaTarefas com o que já tem na const
+    if (listaTarefas) {    //verifica se a const já tem algum item, se sim add nova tarefa
+        const novaLista = JSON.parse(listaTarefas); //converte a string em array
+        novaLista.push(novaTarefa);  //adiciona nova tarefa no array
+        localStorage.setItem('lista-tarefas', JSON.stringify(novaLista)); 
+        //adiciona no localStorage já convertendo de volta para string (localStorage só trabalha com string)
+    } else {  //se não, seta o 1º item
+        localStorage.setItem('lista-tarefas', JSON.stringify([novaTarefa]));
+    }
 }
 
-/*incluir opção de excluir tarefa  
-
-    <div id="${id}">
-                            <p>Tarefa</p>
-                            <input type="checkbox" />
-                            <button imgicontrash ></button>
-                        </div>` 
-*/
+exibirLista();
