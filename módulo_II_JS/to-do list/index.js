97347c57cfa9601d7a04549fba0aadefdc1779c1
devluisapaim <luisac.paim@gmail.com>
2022-10-25 20:54:51 -0300

    Removendo tarefas

--- módulo_II_JS/to-do list/index.js
+++ módulo_II_JS/to-do list/index.js
@@ -1,46 +1,47 @@
 let id = 0;
 
-/* function adicionarTarefa() {
-    id++;
-    const tarefa = document.createElement('div');
-    const nomeTarefa = document.createElement('p');
-    const checkbox = document.createElement('input');
-
-    tarefa.innerHTML = 'Tarefa';
-    tarefa.id = id;
-    checkbox.type = 'checkbox';
-    
-    tarefa.appendChild(nomeTarefa);
-    tarefa.appendChild(checkbox);
-    document.querySelector('#lista-tarefas').appendChild(tarefa);
-} */
-
-/*const tarefa = ` <div>
-<p>Tarefa</p>
-<input type="checkbox" />
-</div>` */
-
-const tarefa = (id, novaTarefa) => ` <div id="${id}">
-                            <p>${novaTarefa}</p>
-                            <input type="checkbox" />
-                        </div>` 
+const tarefa = (id, novaTarefa) => `<div>
+                                        <p id='${id}'>${novaTarefa}</p>
+                                        <input type="checkbox" />
+                                        <button onclick="removerTarefa(${id})">Remover</button>
+                                    </div>`
 
 function exibirLista() {
-    id++;
+    console.log('Called')
     const tarefas = JSON.parse(localStorage.getItem('lista-tarefas'));
     if (tarefas) {
         tarefas.forEach(tarefaListada => {
-            document.querySelector('#tasksList').innerHTML += tarefa(id, tarefaListada)
+            id++;
+            document.querySelector('#tasksList').innerHTML += tarefa(id, tarefaListada);
         });
     }
+}
+
+const validarTarefa = (novaTarefa) => {
+    let tarefaExistente = false;
+    const listaTarefas = JSON.parse(localStorage.getItem('lista-tarefas'));
+    
+    listaTarefas.map(tarefa => {
+        if (tarefa == novaTarefa) {
+            tarefaExistente = true;
+            alert('Tarefa já existente.')
+        }
+    })
+
+    return tarefaExistente;
 }
 
 function adicionarTarefa() {
     id++;
     const novaTarefa = document.getElementById('nome-tarefa').value;
-    document.querySelector('#tasksList').innerHTML += tarefa(id, novaTarefa);
     //'persiste as tarefas': add em localStorage
     const listaTarefas = localStorage.getItem('lista-tarefas'); //cria listaTarefas com o que já tem na const
+    if (validarTarefa(novaTarefa)) {
+        return;
+    }
+    
+    document.querySelector('#tasksList').innerHTML += tarefa(id, novaTarefa);
+
     if (listaTarefas) {    //verifica se a const já tem algum item, se sim add nova tarefa
         const novaLista = JSON.parse(listaTarefas); //converte a string em array
         novaLista.push(novaTarefa);  //adiciona nova tarefa no array
@@ -49,6 +50,15 @@ function adicionarTarefa() {
     } else {  //se não, seta o 1º item
         localStorage.setItem('lista-tarefas', JSON.stringify([novaTarefa]));
     }
+}
+
+const removerTarefa = (id) => {
+    const tarefaDeletada = document.getElementById(id).innerHTML;
+    const listaTarefas = JSON.parse(localStorage.getItem('lista-tarefas'));
+    const novaListaTarefa = listaTarefas.filter(tarefa => tarefa != tarefaDeletada);
+    localStorage.setItem('lista-tarefas', JSON.stringify(novaListaTarefa));
+    document.querySelector('#lista-tarefas').innerHTML = '';
+    exibirLista();
 }
 
 exibirLista();
